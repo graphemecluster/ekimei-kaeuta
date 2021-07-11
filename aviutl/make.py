13ef13badfbec31add1e68774ac903f090b37c41
graphemecluster <graphemecluster@gmail.com>
2021-07-11 09:25:13 +0800

    Update make.py

--- aviutl/make.py
+++ aviutl/make.py
@@ -3,32 +3,56 @@ import csv
 import configparser
 import os.path
 from collections import Counter
+from tkinter import Tk, filedialog, messagebox
 
-utau = configparser.RawConfigParser(comment_prefixes=("UST", "#", ";"))
-utau.optionxform = str
-utau.read("project.ust")
-
-# 駅名,読み,県,会社+路線,地図
-with open("table.csv", newline="") as file:
-    temp = list(csv.reader(file))
-
-data = []
-kansou = []
-for item in temp:
-    if not item or not len(item) or not item[0]:
-        kansou += [len(data)]
-        continue
-    if item[0][-1] == ")":
-        item[0] = item[0].replace("(", "<s80> (")
-    elif item[0][-1] == "]":
-        item[0] = item[0][:item[0].index("[")]
-    data += [item]
-
+# これらの変数は必要に応じて変えてください
+colors = ["ffffff", "ff0000", "00ff00", "0000ff", "ffc000", "0080ff", "ff0080", "c000ff",
+          "804000", "200080", "8080ff", "ff8080", "80ff80", "e080ff", "ffe080", "80e0ff"]
+prefs = ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県",
+         "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県",
+         "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県",
+         "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"]
 fps = 60
 template = """%s
 <p+0,200><#ffff00><s80,ヒラギノ角ゴ ProN W3>%s
 <p+0,+10><#80ffc0><s64,ヒラギノ丸ゴ ProN W4>%s
 <p+0,+5><#ff80ff><s64,ヒラギノ丸ゴ ProN W4>%s"""
+mask = os.path.abspath("../垰瀬内新雛形地図マスク4.png")
+
+Tk().withdraw()
+utaus = []
+for filename in filedialog.askopenfilenames(
+    title="UTAU ファイルを開く（複数選択可）", filetypes=[("UTAU Sequence Text", "*.ust"), ("すべてのファイル", "*.*")], defaultextension=".ust"
+):
+    utau = configparser.RawConfigParser(comment_prefixes=("UST", "#", ";"))
+    utau.optionxform = str
+    utau.read(filename)
+    utaus += [utau]
+
+if not len(utaus):
+    exit()
+
+data = []
+kansou = []
+filename = filedialog.askopenfilename(
+    title="CSV ファイルを開く", filetypes=[("Comma-Separated Values", "*.csv"), ("すべてのファイル", "*.*")], defaultextension=".csv"
+)
+if not filename:
+    exit()
+
+with open(filename, newline="") as file:
+    for item in csv.reader(file):
+        if not item or not len(item) or not item[0]:
+            kansou += [len(data)]
+            continue
+        name = item[0]
+        if name[-1] == ")":
+            name = name.replace("(", "<s80> (")
+        elif name[-1] == "]":
+            name = name[:name.index("[")]
+        assert item[2] in prefs, f"#{len(data)} {name.replace('<s80> ', '')}（{item[1]}）：存在しない県を検出しました：{item[2]}"
+        data += [(name, item[1], item[2], item[5] + " " + item[6]
+                  if item[6] else item[5], item[4].rjust(3, "0"))]
 
 output = configparser.RawConfigParser()
 output.optionxform = str
@@ -42,14 +66,8 @@ output["exedit"] = {
     "audio_ch": 2,
 }
 
+num_prefs = len(prefs)
 multipler = fps / utau.getfloat("#SETTING", "Tempo") / 8
-
-colors = ["ffffff", "ff0000", "00ff00", "0000ff", "ffc000", "0080ff", "ff0080", "c000ff",
-          "804000", "200080", "8080ff", "ff8080", "80ff80", "e080ff", "ffe080", "80e0ff"]
-prefs = ["北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県",
-         "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県",
-         "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県", "徳島県",
-         "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"]
 counter = Counter({pref: 0 for pref in prefs})
 
 j = 0
@@ -71,12 +89,7 @@ def tohex(text):
 
 
 def map_num(index):
-    if not index:
-        return "000"
-    try:
-        return "%03d" % int(data[index - 1][-1])
-    except ValueError:
-        return data[index - 1][-1]
+    return data[index - 1][-1] if index else "000"
 
 
 def write_text(index, start, end):
@@ -109,7 +122,7 @@ def write_text(index, start, end):
             "_name": "マスク",
             "元のサイズに合わせる": 1,
             "type": 0,
-            "name": "*" + os.path.abspath("../垰瀬内新雛形地図マスク4.png"),
+            "name": "*" + mask,
             "mode": 0,
         }
         output.add_section("%d.2" % j)
@@ -145,7 +158,7 @@ def write_text(index, start, end):
         "color2": "000000",
         "align": 1,  # 中央揃え[中]
         "font": "ヒラギノ角ゴ ProN W3",
-        "text": tohex(template % tuple(data[index][:-1])),
+        "text": tohex(template % data[index][:-1]),
     }
     output.add_section("%d.1" % j)
     output["%d.1" % j] = {
@@ -213,14 +226,13 @@ def write_text(index, start, end):
         # 制覇まで残り数回
         while count[nokori][1] == highest:
             nokori += 1
-        if nokori >= 37 and (highest not in prev_yokoku or prev_yokoku[highest] != nokori):
+        if nokori >= num_prefs - 10 and (highest not in prev_yokoku or prev_yokoku[highest] != nokori):
             append = ""
-            if nokori >= 43:
-                append = "(" + "･".join(
-                    map(pref_abbr, next(zip(*count[nokori:])))) + ")"
-                if nokori == 43:
-                    append = "<s40>" + append + "<s>"
-            yokoku += f"<#{colors[highest]}>{highest}周制覇まで残り{47 - nokori}県{append}\r\n"
+            if nokori >= num_prefs - 4:
+                append = f"({'･'.join(map(pref_abbr, next(zip(*count[nokori:]))))})"
+                if nokori == num_prefs - 4:
+                    append = f"<s40>{append}<s>"
+            yokoku += f"<#{colors[highest]}>{highest}周制覇まで残り{num_prefs - nokori}県{append}\r\n"
             prev_yokoku[highest] = nokori
         highest -= 1
 
@@ -264,11 +276,9 @@ def write_kansou(start, end):
     while highest > lowest:
         while count[nokori][1] == highest:
             nokori += 1
-        append = ""
-        if nokori >= 43:
-            append = "\r\n  (" + "･".join(
-                map(pref_abbr, next(zip(*count[nokori:])))) + ")"
-        yokoku += f"<#{colors[highest]}>■全県{highest}周制覇まで残り{47 - nokori}県{append}\r\n"
+        yokoku += f"<#{colors[highest]}>■全県{highest}周制覇まで残り{num_prefs - nokori}県\r\n"
+        if nokori >= num_prefs - 4:
+            yokoku += f"  ({'･'.join(map(pref_abbr, next(zip(*count[nokori:]))))})\r\n"
         highest -= 1
     while highest:
         yokoku += f"<#{colors[highest]}>■全県{highest}周制覇完了\r\n"
@@ -317,7 +327,7 @@ for index, item in enumerate(data):
     while c < len(reading):
         start = c
         norm = reading[c]
-        if reading[c] in "ーっん":
+        if norm in "ーっん":
             vowel = -1
             c += 1
         else:
@@ -326,7 +336,7 @@ for index, item in enumerate(data):
                 c += 1
             except Exception:
                 raise ValueError(
-                    f"#{index} {item[0]}（{reading}）：該当しない文字を検出しました：{reading[c]}")
+                    f"#{index} {item[0].replace('<s80> ', '')}（{reading}）：該当しない文字を検出しました：{reading[c]}")
             else:
                 while True:
                     try:
@@ -353,50 +363,55 @@ for index, item in enumerate(data):
 
 lyrics = []
 
-i = 0
-time = 0
-section = "#%04d" % i
-while utau.has_section(section):
-    lyric = utau.get(section, "Lyric")
-    c = 0
-    norm = lyric[c]
-    if lyric[c] in "ーっん":
-        vowel = -1
-        c += 1
-    else:
-        try:
-            vowel = kanas.index(lyric[c]) % 5
-            c += 1
-        except Exception:
-            pass
-        else:
-            while True:
+for utau in utaus:
+    i = 0
+    time = 0
+    section = "#%04d" % i
+    while utau.has_section(section):
+        lyric = utau.get(section, "Lyric")
+        c = 0
+        if len(lyric):
+            norm = lyric[c]
+            if norm in "ーっん":
+                vowel = -1
+                c += 1
+            else:
                 try:
-                    assert lyric[c] != " "
-                    normalize = (vowel == 1 and lyric[c] in kaiyouon) or (
-                        vowel == 2 and lyric[c] in gouyouon)
-                    vowel = sutegana.index(lyric[c]) % 5
-                    norm += youon[vowel] if normalize else lyric[c]
+                    vowel = kanas.index(lyric[c]) % 5
                     c += 1
                 except Exception:
-                    break
-            for left, right in replacement:
-                if norm.startswith(left):
-                    norm = right + norm[len(left):]
-                    break
-    end = time + utau.getint(section, "Length")
-    if c > 0:
-        lyrics += [{
-            "index": i,
-            "mora": lyric[:c],
-            "norm": norm,
-            "vowel": vowel,
-            "start": time,
-            "end": end
-        }]
-    time = end
-    i += 1
-    section = "#%04d" % i
+                    pass
+                else:
+                    while True:
+                        try:
+                            assert lyric[c] != " "
+                            normalize = (vowel == 1 and lyric[c] in kaiyouon) or (
+                                vowel == 2 and lyric[c] in gouyouon)
+                            vowel = sutegana.index(lyric[c]) % 5
+                            norm += youon[vowel] if normalize else lyric[c]
+                            c += 1
+                        except Exception:
+                            break
+                    for left, right in replacement:
+                        if norm.startswith(left):
+                            norm = right + norm[len(left):]
+                            break
+        end = time + utau.getint(section, "Length")
+        if c:
+            lyrics += [{
+                "index": i,
+                "mora": lyric[:c],
+                "norm": norm,
+                "vowel": vowel,
+                "start": time,
+                "end": end
+            }]
+        time = end
+        i += 1
+        section = "#%04d" % i
+
+if len(utaus) > 1:
+    lyrics = sorted(lyrics, key=lambda item: item["start"])
 
 
 def skippable(a, b):
@@ -443,12 +458,12 @@ while li < len(lyrics) and si < len(stations):
                 si += 1
                 continue
         raise ValueError(
-            f"{data[index][0]}（{data[index][1]}）：歌詞と駅名が一致しません：#{lyrics[li]['index']:04d} {lyrics[li]['mora']} vs {stations[si]['mora']}")
+            f"#{index} {data[index][0]}（{data[index][1]}）：歌詞と駅名が一致しません：{lyrics[li]['mora']} vs {stations[si]['mora']}")
     if si and index != stations[si]["index"]:
         write_text(index, start, lyrics[li - 1]["end"])
         start = lyrics[li]["start"]
         index += 1
-        assert index == stations[si]["index"], f"{data[index][0]}（{data[index][1]}）：駅が飛ばされました"
+        assert index == stations[si]["index"], f"#{index} {data[index][0]}（{data[index][1]}）：駅が飛ばされました"
     li += 1
     si += 1
 write_text(index, start, lyrics[li - 1]["end"])
@@ -474,7 +489,7 @@ if curr_map and prev_map != curr_map:
         "_name": "マスク",
         "元のサイズに合わせる": 1,
         "type": 0,
-        "name": "*" + os.path.abspath("../垰瀬内新雛形地図マスク4.png"),
+        "name": "*" + mask,
         "mode": 0,
     }
     output.add_section("%d.2" % j)
@@ -488,9 +503,15 @@ if curr_map and prev_map != curr_map:
 
 output["exedit"]["length"] = str(last)
 
-with open("object.exo", "w") as file:
-    output.write(file, space_around_delimiters=False)
+if li < len(lyrics) and messagebox.askquestion("確認", "歌詞が余っています。それでも続行しますか？") == 'no' or \
+        si < len(stations) and messagebox.askquestion("確認", "駅名が余っています。それでも続行しますか？") == 'no':
+    exit()
 
-assert li >= len(lyrics), "ファイルは正常に書き出しましたが歌詞が余っています"
-assert si >= len(stations), "ファイルは正常に書き出しましたが駅名が余っています"
-print("ファイルは正常に書き出しました")
+filename = filedialog.asksaveasfilename(
+    title="EXO ファイルを保存", filetypes=[("AviUtl External Object", "*.exo"), ("すべてのファイル", "*.*")], defaultextension=".exo"
+)
+if not filename:
+    exit()
+
+with open(filename, "w") as file:
+    output.write(file, space_around_delimiters=False)
